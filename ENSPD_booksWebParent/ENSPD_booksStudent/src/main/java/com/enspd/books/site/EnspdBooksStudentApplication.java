f3d93615d7f0f3eb4bffd04f0b5fb8197eaaf016
Ebanga Loic <ebangaarnaud@gmail.com>
2023-12-11 11:17:08 +0100

    Project multi moduce ENSPD BOOKS

---
+++ ENSPD_booksWebParent/ENSPD_booksStudent/src/main/java/com/enspd/books/site/EnspdBooksStudentApplication.java
@@ -0,0 +1,13 @@
+package com.enspd.books.site;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EnspdBooksStudentApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EnspdBooksStudentApplication.class, args);
+	}
+
+}
