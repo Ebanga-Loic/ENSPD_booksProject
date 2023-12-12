afbb91428a00af6139d0eefa46f9f06bc7d58e1e
Ebanga Loic <ebangaarnaud@gmail.com>
2023-12-12 22:57:33 +0100

    Fonction liste des utilisateurs

---
+++ ENSPD_booksWebParent/ENSPD_booksAdmin/src/main/java/com/enspd/books/admin/user/UserService.java
@@ -0,0 +1,19 @@
+package com.enspd.books.admin.user;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.enspd.books.common.entity.User;
+
+@Service
+public class UserService {
+
+	@Autowired
+	private UserRepository repo;
+
+	public List<User> listAll() {
+		return (List<User>) repo.findAll();
+	}
+}
