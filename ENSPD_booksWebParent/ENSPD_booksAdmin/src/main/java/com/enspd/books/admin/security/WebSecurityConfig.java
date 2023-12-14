e94d0e242f5cd7ea693105f032212cca38b1e7b2
Ebanga Loic <ebangaarnaud@gmail.com>
2023-12-14 10:21:29 +0100

    Fonction se souvenir de moi

--- ENSPD_booksWebParent/ENSPD_booksAdmin/src/main/java/com/enspd/books/admin/security/WebSecurityConfig.java
+++ ENSPD_booksWebParent/ENSPD_booksAdmin/src/main/java/com/enspd/books/admin/security/WebSecurityConfig.java
@@ -42,7 +42,9 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
 	@Override
 	protected void configure(HttpSecurity http) throws Exception {
 		http.authorizeRequests().anyRequest().authenticated().and().formLogin().loginPage("/login")
-				.usernameParameter("email").permitAll().and().logout().permitAll();
+				.usernameParameter("email").permitAll().and().logout().permitAll().and().rememberMe()
+				.key("AbcDefgHijKlmnOpqrs_1234567890").tokenValiditySeconds(7 * 24 * 60 * 60);
+		;
 	}
 
 	@Override
