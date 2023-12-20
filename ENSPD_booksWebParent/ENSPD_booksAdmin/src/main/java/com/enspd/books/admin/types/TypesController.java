6c17ef755917b38a060ad11b083c3f20e9f678d8
Ebanga Loic <ebangaarnaud@gmail.com>
2023-12-20 01:38:12 +0100

    Formulaire de création d'un nouveau type de livre

--- ENSPD_booksWebParent/ENSPD_booksAdmin/src/main/java/com/enspd/books/admin/types/TypesController.java
+++ ENSPD_booksWebParent/ENSPD_booksAdmin/src/main/java/com/enspd/books/admin/types/TypesController.java
@@ -7,6 +7,8 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
+import com.enspd.books.admin.filieres.FilieresService;
+import com.enspd.books.common.entity.Filieres;
 import com.enspd.books.common.entity.Types;
 
 @Controller
@@ -15,11 +17,25 @@ public class TypesController {
 	@Autowired
 	private TypesService service;
 
+	@Autowired
+	private FilieresService filieresService;
+
 	@GetMapping("/types")
 	public String listAll(Model model) {
 		List<Types> listTypes = service.listAll();
 		model.addAttribute("listTypes", listTypes);
 
 		return "types/types";
+	}
+
+	@GetMapping("/types/new")
+	public String newBrand(Model model) {
+		List<Filieres> listFilieres = filieresService.listAll();
+
+		model.addAttribute("listFilieres", listFilieres);
+		model.addAttribute("types", new Types());
+		model.addAttribute("pageTitle", "Créer un nouveau type de livre");
+
+		return "types/type_form";
 	}
 }
