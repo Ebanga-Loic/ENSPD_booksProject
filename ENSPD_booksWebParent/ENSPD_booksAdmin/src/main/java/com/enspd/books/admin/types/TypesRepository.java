82ff3e80c22a9ba9bc19ab92c37d7e64f7405985
Ebanga Loic <ebangaarnaud@gmail.com>
2023-12-20 09:47:24 +0100

    Pagination liste de type de livre

--- ENSPD_booksWebParent/ENSPD_booksAdmin/src/main/java/com/enspd/books/admin/types/TypesRepository.java
+++ ENSPD_booksWebParent/ENSPD_booksAdmin/src/main/java/com/enspd/books/admin/types/TypesRepository.java
@@ -1,5 +1,8 @@
 package com.enspd.books.admin.types;
 
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.PagingAndSortingRepository;
 
 import com.enspd.books.common.entity.Types;
@@ -7,7 +10,10 @@ import com.enspd.books.common.entity.Types;
 public interface TypesRepository extends PagingAndSortingRepository<Types, Integer> {
 
 	public Long countById(Integer id);
-	
+
 	public Types findByName(String name);
+
+	@Query("SELECT t FROM Types t WHERE t.name LIKE %?1%")
+	public Page<Types> findAll(String keyword, Pageable pageable);
 
 }
