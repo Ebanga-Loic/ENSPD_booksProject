cbe210c8b8a6d10857732e4788e48bb2eb7718df
Ebanga Loic <ebangaarnaud@gmail.com>
2023-12-20 12:40:33 +0100

    Entité Book et repository

---
+++ ENSPD_booksCommon/src/main/java/com/enspd/books/common/entity/Book.java
@@ -0,0 +1,118 @@
+package com.enspd.books.common.entity;
+
+import java.util.Date;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "books")
+public class Book {
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Integer id;
+
+	@Column(unique = true, length = 256, nullable = false)
+	private String name;
+
+	@Column(length = 4096, nullable = false, name = "full_description")
+	private String fullDescription;
+
+	@Column(name = "created_time")
+	private Date createdTime;
+
+	@Column(name = "updated_time")
+	private Date updatedTime;
+
+	private boolean enabled;
+
+	@Column(name = "in_stock")
+	private boolean inStock;
+
+	@ManyToOne
+	@JoinColumn(name = "filiere_id")
+	private Filieres filieres;
+
+	@ManyToOne
+	@JoinColumn(name = "type_id")
+	private Types types;
+
+	public Integer getId() {
+		return id;
+	}
+
+	public void setId(Integer id) {
+		this.id = id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getFullDescription() {
+		return fullDescription;
+	}
+
+	public void setFullDescription(String fullDescription) {
+		this.fullDescription = fullDescription;
+	}
+
+	public Date getCreatedTime() {
+		return createdTime;
+	}
+
+	public void setCreatedTime(Date createdTime) {
+		this.createdTime = createdTime;
+	}
+
+	public Date getUpdatedTime() {
+		return updatedTime;
+	}
+
+	public void setUpdatedTime(Date updatedTime) {
+		this.updatedTime = updatedTime;
+	}
+
+	public boolean isEnabled() {
+		return enabled;
+	}
+
+	public void setEnabled(boolean enabled) {
+		this.enabled = enabled;
+	}
+
+	public boolean isInStock() {
+		return inStock;
+	}
+
+	public void setInStock(boolean inStock) {
+		this.inStock = inStock;
+	}
+
+	public Filieres getFilieres() {
+		return filieres;
+	}
+
+	public void setFilieres(Filieres filieres) {
+		this.filieres = filieres;
+	}
+
+	public Types getTypes() {
+		return types;
+	}
+
+	public void setTypes(Types types) {
+		this.types = types;
+	}
+
+}
