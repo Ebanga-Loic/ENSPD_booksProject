1d9ecf33a39a8c74b4857a541830357df8b6920f
Ebanga Loic <ebangaarnaud@gmail.com>
2024-01-05 22:33:29 +0100

    Mettre à jour l'état du livre et supprimer le livre

---
+++ ENSPD_booksWebParent/ENSPD_booksAdmin/src/main/java/com/enspd/books/admin/books/BookNotFoundException.java
@@ -0,0 +1,9 @@
+package com.enspd.books.admin.books;
+
+public class BookNotFoundException extends Exception {
+
+	public BookNotFoundException(String message) {
+		super(message);
+	}
+
+}
--- ENSPD_booksWebParent/ENSPD_booksAdmin/src/main/java/com/enspd/books/admin/books/BookRepository.java
+++ ENSPD_booksWebParent/ENSPD_booksAdmin/src/main/java/com/enspd/books/admin/books/BookRepository.java
@@ -1,5 +1,7 @@
 package com.enspd.books.admin.books;
 
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.PagingAndSortingRepository;
 
 import com.enspd.books.common.entity.Book;
@@ -7,5 +9,11 @@ import com.enspd.books.common.entity.Book;
 public interface BookRepository extends PagingAndSortingRepository<Book, Integer> {
 
 	public Book findByName(String name);
+
+	@Query("UPDATE Book b SET b.enabled = ?2 WHERE b.id = ?1")
+	@Modifying
+	public void updateEnabledStatus(Integer id, boolean enabled);
+
+	public Long countById(Integer id);
 
 }
