2b6d6ddb323dda93d945e6ed162417e5dc730369
Ebanga Loic <ebangaarnaud@gmail.com>
2024-01-07 02:58:50 +0100

    Pagination liste de livre

--- ENSPD_booksWebParent/ENSPD_booksAdmin/src/main/java/com/enspd/books/admin/books/BookController.java
+++ ENSPD_booksWebParent/ENSPD_booksAdmin/src/main/java/com/enspd/books/admin/books/BookController.java
@@ -3,6 +3,8 @@ package com.enspd.books.admin.books;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -24,9 +26,33 @@ public class BookController {
 	private TypesService typeService;
 
 	@GetMapping("/books")
-	public String listAll(Model model) {
-		List<Book> listBooks = bookService.listAll();
+	public String listFirstPage(Model model) {
+		return listByPage(1, model, "name", "asc", null);
+	}
 
+	@GetMapping("/books/page/{pageNum}")
+	public String listByPage(@PathVariable(name = "pageNum") int pageNum, Model model,
+			@Param("sortField") String sortField, @Param("sortDir") String sortDir, @Param("keyword") String keyword) {
+		Page<Book> page = bookService.listByPage(pageNum, sortField, sortDir, keyword);
+		List<Book> listBooks = page.getContent();
+
+		long startCount = (pageNum - 1) * BookService.BOOKS_PER_PAGE + 1;
+		long endCount = startCount + BookService.BOOKS_PER_PAGE - 1;
+		if (endCount > page.getTotalElements()) {
+			endCount = page.getTotalElements();
+		}
+
+		String reverseSortDir = sortDir.equals("asc") ? "desc" : "asc";
+
+		model.addAttribute("currentPage", pageNum);
+		model.addAttribute("totalPages", page.getTotalPages());
+		model.addAttribute("startCount", startCount);
+		model.addAttribute("endCount", endCount);
+		model.addAttribute("totalItems", page.getTotalElements());
+		model.addAttribute("sortField", sortField);
+		model.addAttribute("sortDir", sortDir);
+		model.addAttribute("reverseSortDir", reverseSortDir);
+		model.addAttribute("keyword", keyword);
 		model.addAttribute("listBooks", listBooks);
 
 		return "books/books";
--- ENSPD_booksWebParent/ENSPD_booksAdmin/src/main/java/com/enspd/books/admin/books/BookRepository.java
+++ ENSPD_booksWebParent/ENSPD_booksAdmin/src/main/java/com/enspd/books/admin/books/BookRepository.java
@@ -1,5 +1,7 @@
 package com.enspd.books.admin.books;
 
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.PagingAndSortingRepository;
@@ -15,5 +17,9 @@ public interface BookRepository extends PagingAndSortingRepository<Book, Integer
 	public void updateEnabledStatus(Integer id, boolean enabled);
 
 	public Long countById(Integer id);
+
+	@Query("SELECT p FROM Book p WHERE p.name LIKE %?1% " + "OR p.fullDescription LIKE %?1% " + "OR p.auteur LIKE %?1% "
+			+ "OR p.types.name LIKE %?1% " + "OR p.filieres.name LIKE %?1%")
+	public Page<Book> findAll(String keyword, Pageable pageable);
 
 }
