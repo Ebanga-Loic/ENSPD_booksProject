2bfd7bde47026c19a912b3d6c3b8a2f7365138cf
Ebanga Loic <ebangaarnaud@gmail.com>
2024-01-07 08:50:59 +0100

    Filtrer les livres par filiere et rendre la page de liste de livre responsive

---
+++ ENSPD_booksWebParent/ENSPD_booksAdmin/bin/src/main/resources/static/js/common_list.js
@@ -0,0 +1,12 @@
+function clearFilter() {
+	window.location = moduleURL;
+}
+
+function showDeleteConfirmModal(link, entityName) {
+	entityId = link.attr("entityId");
+
+	$("#yesButton").attr("href", link.attr("href"));
+	$("#confirmText").text("Etes-vous sûr de vouloir supprimer ceci "
+		+ entityName + " ID " + entityId + "?");
+	$("#confirmModal").modal();
+}
